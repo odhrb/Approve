e9a249a01c4aac057b35b3660c9413069b36de06
odhrb <kishankumare24@gmail.com>
2025-07-20 21:54:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-# configuration variables
-API_ID = ''
-API_HASH = ''
-BOT_TOKEN = ''
-MONGO_URI = ''
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+MONGO_URI = os.environ.get("MONGO_URI")
+ADMIN_IDS = list(map(int, os.environ.get("ADMIN_IDS", "").split()))
+START_PIC = os.environ.get("START_PIC", "")
+APPROVAL_NOTE = os.environ.get("APPROVAL_NOTE", "You have been approved!")
